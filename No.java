c14320a1bc69a32153cbe0563a91df9033ae6fbf
gustavo <gustavomonteiro7865@gmail.com>
2025-11-11 11:15:45 -0300

    Implementa classe No com atributos( altura, id, direita, esquerda),  métodos( get e set) e construtor

--- No.java
+++ No.java
@@ -1,3 +1,45 @@
 public class No {
+    private int id;
+    private No esquerda;
+    private No direita;
+    private int altura;
     
+    public No(int id) {
+        this.id = id;
+        this.esquerda = null;
+        this.direita = null;
+        this.altura = 1;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public No getEsquerda() {
+        return this.esquerda;
+    }
+    
+    public void setEsquerda(No esquerda) {
+        this.esquerda = esquerda;
+    }
+    
+    public No getDireita() {
+        return this.direita;
+    }
+    
+    public void setDireita(No direita) {
+        this.direita = direita;
+    }
+    
+    public int getAltura() {
+        return this.altura;
+    }
+    
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
 }
