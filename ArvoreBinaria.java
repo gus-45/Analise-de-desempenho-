5a619dd6a5cdd17b5902fd02094befe885716025
gustavo <gustavomonteiro7865@gmail.com>
2025-11-13 14:59:30 -0300

    adicionando a classe arvoreBinaria com métodos de busca e inserção

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -1,3 +1,54 @@
 public class ArvoreBinaria {
+    private No raiz;
     
+    public void inserir(int id) {
+        if(this.raiz == null) {
+            this.raiz = new No(id);
+        } else {
+            inserir(id, raiz);
+        }
+    }
+    
+    private void inserir(int id, No raiz) {
+        if(id == raiz.getId()) {
+            return;
+        }
+        if(id < raiz.getId()) {
+            if(raiz.getEsquerda() == null) {
+                No novo = new No(id);
+                raiz.setEsquerda(novo);
+            } else {
+                inserir(id, raiz.getEsquerda());
+            }
+        } else {
+            if(raiz.getDireita() == null) {
+                No novo = new No(id);
+                raiz.setDireita(novo);
+            } else {
+                inserir(id, raiz.getDireita());
+            }
+        }
+    }
+    
+    public boolean buscar(int id) {
+        if(this.raiz == null) {
+            return false;
+        } else {
+            return buscar(id, raiz);
+        }
+    }
+    
+    private boolean buscar(int id, No raiz) {
+        if(raiz == null) {
+            return false;
+        }
+        if(id == raiz.getId()) {
+            return true;
+        }
+        if(id < raiz.getId()) {
+            return buscar(id, raiz.getEsquerda());
+        } else {
+            return buscar(id, raiz.getDireita());
+        }
+    }
 }
